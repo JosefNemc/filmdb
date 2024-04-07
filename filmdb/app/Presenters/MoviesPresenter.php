b6c14d377da6ad247277f404408b57daf364f825
noro <foto@josefnemec.cz>
2024-04-08 00:15:35 +0200

    First Commit

--- filmdb/app/Presenters/MoviesPresenter.php
+++ filmdb/app/Presenters/MoviesPresenter.php
@@ -30,74 +30,80 @@ class MoviesPresenter extends Presenter
 
     public function actionDefault(int $id = null) //bez parametru vrátí seznam všech filmů
     {
+        $moviesData = [];
+
 
         if ($this->getHttpRequest()->getHeader('Authorization') !== $this->token)
         {
-                //Autorizace v pořádku máme GET
-                if ($this->getHttpRequest()->isMethod('GET') && $id === null) {
-                    $movies = $this->movies->fetchAll();
-                    foreach ($movies as $movie)
-                    {
-                        $moviesData[] = [
-                            'id' => $movie->id,
-                            'name' => $movie->name,
-                            'author' => $movie->author,
-                            'description' => $movie->description
-                        ];
-                    }
+            //Autorizace v pořádku máme GET
+            if ($this->getHttpRequest()->isMethod('GET') && ($id === null))
+            {
+                $movies = $this->movies->fetchAll();
+                foreach ($movies as $movie) {
+                    $moviesData[] = [
+                        'id' => $movie->id,
+                        'name' => $movie->name,
+                        'author' => $movie->author,
+                        'description' => $movie->description
+                    ];
+                }
 
-                    $this->sendJson($moviesData);
+                $this->sendJson($moviesData);
+
+            }
+            else
+            {
+                $movies = $this->movies->fetch($id);
+
+                $moviesData = [
+                    'id' => $movies->id,
+                    'name' => $movies->name,
+                    'author' => $movies->author,
+                    'description' => $movies->description
+                ];
+
+
+                $this->sendJson($moviesData);
+
+            }
+
+
+            if ($this->getHttpRequest()->isMethod('POST') && ($id === null))
+            {
+                $requestData = $this->getHttpRequest()->getPost(); // Získání dat z požadavku
+                if (isset($requestData['name']) && isset($requestData['author']) && isset($requestData['description']))
+                {
+                    $this->movies->create($requestData);
+                    $this->sendJson(['success' => true]);
                 }
                 else
                 {
-                    $movies = $this->movies->fetch($id);
 
-                        $moviesData[] = [
-                            'id' => $movies->id,
-                            'name' => $movies->name,
-                            'author' => $movies->author,
-                            'description' => $movies->description
-                        ];
-
-
-                    $this->sendJson($moviesData);
+                    $this->sendJson(['error' => 'Missing data']);
                 }
 
-            if ($this->getHttpRequest()->isMethod('POST') && $id === null) {
-                $requestData = $this->getHttpRequest()->getPost(); // Získání dat z požadavku
-                if (isset($requestData['name']) && isset($requestData['author']) && isset($requestData['description']))
-                    {
-                       $this->movies->create($requestData);
-                       $this->sendJson(['success' => true]);
-                    }
-                else
-                    {
-
-                       $this->sendJson(['error' => 'Missing data']);
-                    }
-
             }
-            elseif($this->getHttpRequest()->isMethod('PUT') && $id !== null)
+            elseif ($this->getHttpRequest()->isMethod('PUT') && ($id !== null))
             {
                 $requestData = $this->getHttpRequest()->getPost(); // Získání dat z POST požadavku
                 if (isset($requestData['name']) && isset($requestData['author']) && isset($requestData['description']) && ($id !== null))
-                    {
-                        $this->movies->update($id,$requestData);
-                        $this->sendJson(['success' => true]);
-                    }
-                else
-                    {
-                       $this->sendJson(['error' => 'Missing data']);
-                    }
+                {
+                    $this->movies->update($id, $requestData);
+                    $this->sendJson(['success' => true]);
                 }
-            elseif($this->getHttpRequest()->isMethod('DELETE') && $id !== null)
+                else
+                {
+                    $this->sendJson(['error' => 'Missing data']);
+                }
+            }
+            elseif ($this->getHttpRequest()->isMethod('DELETE') && ($id !== null))
             {
                 $this->movies->delete($id);
                 $this->sendJson(['success' => true]);
             }
 
-        }
 
+        }
         else
         {
 
